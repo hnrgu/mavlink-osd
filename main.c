8b6404523979db18eac7bc0bf9d40f750d306171
Henry Gu <henry@ianbr.com>
2022-04-25 00:46:42 -0400

    Initial commit

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <termios.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <common/mavlink.h>
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s [tty]\n", argv[0]);
+		return 1;
+	}
+
+	int fd = open(argv[1], O_RDWR);
+	if (fd == -1) {
+		perror("open");
+		return 1;
+	}
+
+	struct termios tty;
+	if (tcgetattr(fd, &tty) != 0) {
+		perror("tcgetattr");
+		return 1;
+	}
+
+	cfmakeraw(&tty);
+
+	if (tcsetattr(fd, TCSANOW, &tty) != 0) {
+		perror("tcsetattr");
+		return 1;
+	}
+
+	while (1) {
+		char buf[2048];
+		int len = read(fd, buf, sizeof(buf));
+
+		if (len == -1) {
+			perror("read");
+			continue;
+		}
+
+		for (int i = 0; i < len; i ++) {
+			mavlink_status_t status;
+			mavlink_message_t msg;
+
+			if (mavlink_parse_char(MAVLINK_COMM_0, buf[i], &msg, &status)) {
+				printf("received message: %d\n", msg.msgid);
+			}
+		}
+	}
+}
